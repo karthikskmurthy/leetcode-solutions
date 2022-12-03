db155e3a47c5d3e225cb5f1204768cefae805418
Karthik Srinivasamurthy <38109484+karthikskmurthy@users.noreply.github.com>
2022-12-03 22:27:11 +0530

    Create search_insert_position.py

---
+++ search_insert_position.py
@@ -0,0 +1,15 @@
+#Runtime - 54 ms
+#memory - 14.6 MB
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid -1
+            elif nums[mid] < target:
+                start = mid + 1
+        return start
